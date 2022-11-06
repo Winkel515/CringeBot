da0cdb1291f519371ab68decb70691b1b8d86245
Winkel515 <winkel515@hotmail.com>
2022-11-06 17:09:46 -0500

    bot reply and react to stuff

---
+++ main.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits, Emoji } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers],
+});
+
+client.once('ready', () => console.log('Ready!'));
+
+client.on('messageCreate', (message) => {
+  if (message.content.includes('dn')) {
+    message.react('🍆');
+    message.react('💦');
+    return message.reply('deez nuts haha gotem');
+  }
+
+  if(message.content.toLowerCase().includes('who') && message.content.toLowerCase().includes('joe')) {
+    return message.reply('Joe mama haha gotem');
+  }
+
+  if (message.content === '!help') {
+    return message.reply("there's no help lmao");
+  }
+
+  if(message.author.id == '195278304700399616') {
+    return message.react("🐒")
+  }
+});
+
+client.login(process.env.TOKEN);
